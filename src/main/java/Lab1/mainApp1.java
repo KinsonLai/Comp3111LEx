f2d0d036da42f144d4229c8eef1998a6a6f225d0
KinsonLai <lckit1008@gmail.com>
2026-09-19 18:32:57 +0800

    Add two report heading lines

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -2,6 +2,8 @@ package Lab1;
 
 public class mainApp1 {
     public static void main(String[] args) {
+        System.out.println("COMP3111 - Lab 1");
+        System.out.println("Power and factorial calculations");
         int b = 2;
         int n = 11;
         System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
